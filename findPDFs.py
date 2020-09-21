f14f516ae4c3316ddde37f4d4e5f3ebb63dd1835
lbartels <lbart001@odu.edu>
2020-09-20 21:36:47 -0400

    Add PDF finder Python program

---
+++ findPDFs.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import re
+
+# A test to cofirm the command line argument was correctly passed in
+print("URI requested: ", sys.argv[1])
+
+response = requests.get(sys.argv[1])
+
+soup = BeautifulSoup(response.text)
+for links in soup.find_all('a'):
+    resp = requests.get((links.get('href')))
+    if resp.headers['Content-Type'] != "application/pdf":
+        continue
+    else:
+        print('\n')
+        print('URI: ', links.get('href'))
+        print('Final URI: ', resp.url)
+        print('Content Length: ', resp.headers['Content-Length'], 'bytes')
+        print('\n')
